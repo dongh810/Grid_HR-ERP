d1333914f4ad59423410f761ed680553a26da878
동동 <baek0810@naver.com>
2024-05-09 11:42:20 +0900

    Feat: 정책 등록, 삭제기능 추가

--- BaekEnd/grid/src/main/java/org/highfives/grid/vacation/command/controller/VacationController.java
+++ BaekEnd/grid/src/main/java/org/highfives/grid/vacation/command/controller/VacationController.java
@@ -2,6 +2,7 @@ package org.highfives.grid.vacation.command.controller;
 
 import org.highfives.grid.vacation.command.service.VacationService;
 import org.highfives.grid.vacation.command.vo.ModifyPolicy;
+import org.highfives.grid.vacation.command.vo.RegistPolicy;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +20,18 @@ public class VacationController {
         this.modelMapper = modelMapper;
     }
 
-    @PutMapping("/policy/{typeId}")
-    public void modifyVacationPolicy(@RequestBody ModifyPolicy policyInfo, @PathVariable int typeId) {
-        vacationService.modifyVacationPolicy(policyInfo, typeId);
+    @PutMapping("/policy/{id}")
+    public void modifyVacationPolicy(@RequestBody ModifyPolicy policyInfo, @PathVariable int id) {
+        vacationService.modifyVacationPolicy(policyInfo, id);
+    }
 
+    @DeleteMapping("/policy/{id}")
+    public void deleteVacationPolicy(@PathVariable int id) {
+        vacationService.deleteVacationPolicy(id);
+    }
+
+    @PostMapping("/policy")
+    public void registVacationPolicy(@RequestBody RegistPolicy policyInfo) {
+        vacationService.registVacationPolicy(policyInfo);
     }
 }
--- BaekEnd/grid/src/main/java/org/highfives/grid/vacation/command/service/VacationServiceImpl.java
+++ BaekEnd/grid/src/main/java/org/highfives/grid/vacation/command/service/VacationServiceImpl.java
@@ -2,10 +2,10 @@ package org.highfives.grid.vacation.command.service;
 
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
-import org.highfives.grid.vacation.command.dto.VacationPolicyDTO;
 import org.highfives.grid.vacation.command.entity.VacationPolicy;
 import org.highfives.grid.vacation.command.repository.VacationPolicyRepository;
 import org.highfives.grid.vacation.command.vo.ModifyPolicy;
+import org.highfives.grid.vacation.command.vo.RegistPolicy;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +26,23 @@ public class VacationServiceImpl implements VacationService{
 
     @Override
     @Transactional
-    public void modifyVacationPolicy(ModifyPolicy policyInfo, int typeId) {
-        VacationPolicy foundPolicy = vacationPolicyRepository.findById(Long.valueOf(typeId)).orElseThrow(IllegalAccessError::new);
+    public void modifyVacationPolicy(ModifyPolicy policyInfo, int id) {
+        VacationPolicy foundPolicy = vacationPolicyRepository.findById(Long.valueOf(id)).orElseThrow(IllegalAccessError::new);
         foundPolicy.setContent(policyInfo.getContent());
+    }
+
+    @Override
+    @Transactional
+    public void deleteVacationPolicy(int id) {
+        vacationPolicyRepository.deleteById(Long.valueOf(id));
+    }
+
+    @Override
+    @Transactional
+    public void registVacationPolicy(RegistPolicy policyInfo) {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        VacationPolicy vacationPolicy = modelMapper.map(policyInfo, VacationPolicy.class);
+        vacationPolicyRepository.save(vacationPolicy);
     }
 
 
